d09019d5dfb4bc462dd88cc1c2935e2244f1c931
Guy Umbright <gumbright@salesforce.com>
2014-06-18 15:14:08 -0500

    fix a background color, add the show demo to arc/storyboard version

--- expandablemessagearc/expandablemessagearc/ViewController.m
+++ expandablemessagearc/expandablemessagearc/ViewController.m
@@ -54,4 +54,20 @@
     
     [message presentInController:self animated:YES];
 }
+
+- (IBAction) showPressed:(id)sender
+{
+    NSError* error;
+    NSURL* url = [[NSBundle mainBundle] URLForResource:@"AppDelegate" withExtension:@"txt"];
+    NSString* detail = [NSString stringWithContentsOfURL:url
+                                                encoding:NSUTF8StringEncoding error:&error];
+    
+    UIXExpandableMessageController* message = [[UIXExpandableMessageController alloc] initWithTitle:@"My Title"
+                                                                                       shortMessage:@"Something happened and you really want to know more about it"
+                                                                                             detail:detail];
+
+    message.emailSubject = @"Error detail from app";
+    [message show];
+}
+
 @end
